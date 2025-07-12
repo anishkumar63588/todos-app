a321a072a2921adfc00b8e76227467a6ef627386
anish <anishkumar63588@gmail.com>
2025-07-12 21:23:00 +0530

    Add comments

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+# Building a todos app
+
+# Importing necessary modules
 import functions
 import time
 
+# Printing current time
 now = time.strftime("%b %d, %Y %H:%M:%S")
 print("Welcome to to-do app!")
 print("It is ", now)
 
+# Creating a loop to ask user input repeatedly until user exits
 while True:
+
+    # Taking user input
     user_action = input("Type 'add', 'edit', 'show', 'complete', and 'exit': ").lower().strip()
 
+    # Applying a condition to add to-do
     if user_action.startswith("add"):
         todo = user_action[4:]
 
@@ -17,12 +25,14 @@ while True:
 
         functions.write_todos(todos)
 
+    # Applying a condition to show todos
     elif user_action.startswith("show"):
         todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             print(f"{index + 1} - {item.capitalize().strip()}")
 
+    # Applying a condition to edit to-do
     elif user_action.startswith("edit"):
         try:
             number = int(user_action[5:])
@@ -38,6 +48,7 @@ while True:
             print("Your command is not valid.")
             continue
 
+    # Applying a condition to remove the completed to-do
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
@@ -52,10 +63,11 @@ while True:
         except IndexError:
             print("Wrong number of to-do!")
 
+    # Applying a condition to exit the loop
     elif user_action == "exit":
         break
 
     else:
-        print("Wrong command, try again!")
+        print("Wrong command, try again!")  # This will be printed when user will enter wrong input
 
-print("Bye!")
+print("Thanks for using todos app!")
