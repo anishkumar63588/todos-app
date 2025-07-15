83451b0215facb3ef54d2e5134893cabe79560f4
Anish Kumar <anishkumar63588@gmail.com>
2025-07-15 08:58:02 +0530

    Implemented image of "Add" and "Complete" buttons

--- gui.py
+++ gui.py
@@ -2,17 +2,19 @@ import functions
 import FreeSimpleGUI as sg
 import time
 
-sg.theme("NeonYellow1")
+sg.theme("Black")
 clock = sg.Text("", key="clock")
 label_add = sg.Text("Type in a to-do item")
 input_box = sg.InputText(tooltip="Enter item", key="todo")
-add_button = sg.Button("Add", size=5)
+add_button = sg.Button(key="Add", size=5, tooltip="Add item",
+                       image_source="add.png", mouseover_colors="LightBlue2")
 list_box = sg.Listbox(values=functions.get_todos(),
                       key= "todos",
                       size=(45, 10),
                       enable_events=True)
-edit_button = sg.Button("Edit", size=6)
-complete_button = sg.Button("Complete", size=10)
+edit_button = sg.Button("Edit", size=4)
+complete_button = sg.Button(key="Complete", size=10, tooltip="Complete item",
+                            mouseover_colors="LightBlue2", image_source="complete.png")
 exit_button = sg.Button("Exit", size=6)
 
 window = sg.Window(title='Welcome to to-do App',
@@ -25,6 +27,9 @@ window = sg.Window(title='Welcome to to-do App',
 
 while True:
     event, values = window.read(timeout=200)
+    if event == sg.WIN_CLOSED or event == "Exit":
+        break
+
     window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
@@ -57,11 +62,8 @@ while True:
                 window["todo"].update(value="")
             except IndexError:
                 sg.popup("Please select item first.", font=("Helvetica", 20))
-        case "Exit":
-            break
         case "todos":
             window['todo'].update(value=values['todos'][0])
 
-print("Thanks for using To-Do App")
 window.close()
 
